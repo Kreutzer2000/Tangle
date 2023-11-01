a50ed1a0f32a0db48594504839d7797a4b98abab
Kreutzer2000 <71472774+Kreutzer2000@users.noreply.github.com>
2023-11-01 01:18:32 -0500

    Resolviendo conflictos Puerto azure

--- server.js
+++ server.js
@@ -479,6 +479,7 @@ app.post('/login',  async (req, res) => {
 });
 
 // Inicia el servidor en el puerto 3000
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
